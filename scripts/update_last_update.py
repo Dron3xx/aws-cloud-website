79edb5d8524bbd2a7a98bdf82cd68e29783edd8d
Dawid B <dawid.bronikowski@onet.eu>
2026-08-04 05:40:23 +0200

    Format update_last_update.py
    - Applied Ruff formating
    - Changed display_date format

--- scripts/update_last_update.py
+++ scripts/update_last_update.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 def update_footer_metadata():
     # Get the current date in the desired format
     iso_date = dt.datetime.now().strftime("%Y-%m-%d")
-    display_date = dt.datetime.now().strftime("%d-%m-%Y")
+    display_date = dt.datetime.now().strftime("%d/%m/%Y")
 
     # Define the path to the index.html file
     index_file_path = Path(__file__).parent.parent / "index.html"
